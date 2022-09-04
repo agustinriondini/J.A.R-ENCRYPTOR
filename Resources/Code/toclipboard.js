1c166f8a936a9042e65e001dd5ce919efe183f37
Agustin Riondini <105334088+agustinriondini@users.noreply.github.com>
2022-09-04 16:19:40 -0300

    Delete toclipboard.js
    Descontinuado. Los cuadros de textos son bidireccionales

--- Resources/Code/toclipboard.js
+++
@@ -1,9 +0,0 @@
-
-/*/funcion desactivada, boton de entrada removido de index.html
-
-function myFunction() {
-    var copyText = document.getElementById("#outputtext");
-    copyText.select("");
-    navigator.clipboard.writeText(copyText.value);
-    alert("Copiado al portapapeles");
-  }
